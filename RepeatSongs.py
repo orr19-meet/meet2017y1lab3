435fd02931f4576d82bbe92145168348b254ef9d
orr19-meet <orr19@meet.mit.edu>
2017-07-25 12:22:22 +0300

    Stopping here

---
+++ RepeatSongs.py
@@ -0,0 +1,12 @@
+print("Row "*3 + " your boat")
+print("Gently down the stream.")
+print("Merrily "*4)
+print("Life is but a dream.")
+
+
+print("And I was like")
+print("Baby "*3)
+print("oooooooh!")
+print("Baby "*3)
+print("nooooooo!")
+
